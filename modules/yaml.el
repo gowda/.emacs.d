bb3bea44dd17db10f41c29499ecc0141e3379427
Basavanagowda Kanur <basavanagowda@gmail.com>
2020-07-13 10:56:29 +0530

    Add '.yaml' to list of files for yaml-mode

--- modules/yaml.el
+++ modules/yaml.el
@@ -2,11 +2,13 @@
 (use-package yaml-mode
   :ensure t
   :commands yaml-mode
+  :mode
+  (("\\.yaml\\'" . yaml-mode)
+   ("\\.yml\\'" . yaml-mode))
   :init
   (add-hook 'yaml-mode-hook 'subword-mode)
   (add-hook 'yaml-mode-hook
             (lambda()
-              (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
-  (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode)))
+              (define-key yaml-mode-map "\C-m" 'newline-and-indent))))
 
 (provide 'yaml)
